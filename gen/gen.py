d7646c3fcfd6e15bef2d201e4ab4295ed618b15f
Thomas Dybdahl Ahle <thomas@ahle.dk>
2019-11-15 20:42:10 +0100

    Updated license to Affero GPL 3

---
+++ gen/gen.py
@@ -0,0 +1,398 @@
+import sys
+import random
+import string
+import collections
+import itertools
+import argparse
+
+from mitm import Mitm
+
+
+parser = argparse.ArgumentParser(description='Generate Numberlink Puzzles')
+parser.add_argument('width', type=int, default=10,
+                    help='Width of the puzzle')
+parser.add_argument('height', type=int, default=10,
+                    help='Height of the puzzle')
+parser.add_argument('--min', type=int, default=-1,
+                    help='Minimum number of pairs')
+parser.add_argument('--max', type=int, default=-1,
+                    help='Minimum number of pairs')
+parser.add_argument('--verbose', action='store_true',
+                    help='Print progress information')
+parser.add_argument('--solve', action='store_true',
+                    help='Print solution as well as puzzle')
+parser.add_argument('--zero', action='store_true',
+                    help='Print puzzle in zero format')
+args = parser.parse_args()
+
+
+def sign(x):
+    if x == 0: return x
+    return -1 if x < 0 else 1
+
+
+def union(uf, a, b):
+    a_par, b_par = find(uf, a), find(uf, b)
+    uf[a_par] = b_par
+
+
+def find(uf, a):
+    if uf.get(a, a) == a:
+        return a
+    par = find(uf, uf.get(a,a))
+    uf[a] = par # Path compression
+    return par
+
+
+class Grid:
+    def __init__(self, w, h):
+        self.w, self.h = w, h
+        self.grid = {}
+    def __setitem__(self, key, val):
+        self.grid[key] = val
+    def __getitem__(self, key):
+        return self.grid.get(key, ' ')
+    def __repr__(self):
+        res = []
+        for y in range(self.h):
+            res.append(''.join(self[x,y] for x in range(self.w)))
+        return '\n'.join(res)
+    def __iter__(self):
+        return iter(self.grid.items())
+    def __contains__(self, key):
+        return key in self.grid
+    def __delitem__(self, key):
+        del self.grid[key]
+    def add(self, other):
+        if not other:
+            return False
+        assert self.w == other.w and self.h == other.h
+        self.grid = {**self.grid, **other.grid}
+        return True
+
+
+def test_path(grid, path, x0, y0, dx0=0, dy0=1):
+    """ Test whether the path is safe to draw on the grid, starting at x0, y0 """
+    return all(0 <= x0-x+y < grid.w and 0 <= y0+x+y < grid.h
+               and (x0-x+y, y0+x+y) not in grid for x, y in path.xys(dx0, dy0))
+
+
+def draw_path(grid, path, x0, y0, dx0=0, dy0=1, loop=False):
+    """ Draws path on the grid. Asserts this is safe (no overlaps).
+        For non-loops, the first and the last character is not drawn,
+        as we don't know what shape they should have. """
+    ps = list(path.xys(dx0,dy0))
+    # For loops, add the second character, so we get all rotational tripples:
+    # abcda  ->  abcdab  ->  abc, bcd, cda, dab
+    if loop:
+        assert ps[0] == ps[-1], (path, ps)
+        ps.append(ps[1])
+    for i in range(1, len(ps)-1):
+        xp, yp = ps[i-1]
+        x, y = ps[i]
+        xn, yn = ps[i+1]
+        grid[x0-x+y,y0+x+y] = {
+            (1,1,1): '<', (-1,-1,-1): '<',
+            (1,1,-1): '>', (-1,-1,1): '>',
+            (-1,1,1): 'v', (1,-1,-1): 'v',
+            (-1,1,-1): '^', (1,-1,1): '^',
+            (0,2,0): '\\', (0,-2,0): '\\',
+            (2,0,0): '/', (-2,0,0): '/'
+        }[xn-xp, yn-yp, sign((x-xp)*(yn-y)-(xn-x)*(y-yp))]
+
+
+def shrink_grid(grid):
+    small_grid = Grid(grid.w//2, grid.h//2)
+    for y in range(grid.h//2):
+        for x in range(grid.w//2):
+            small_grid[x,y] = grid[2*x+1, 2*y+1]
+    return small_grid
+
+
+def make_tubes(grid):
+    uf = {}
+    tube_grid = Grid(grid.w, grid.h)
+    for x in range(grid.w):
+        d = '-'
+        for y in range(grid.h):
+            # We union things down and to the right.
+            # This means ┌ gets to union twice.
+            for dx, dy in {
+                    '/-':[(0,1)], '\\-':[(1,0),(0,1)],
+                    '/|':[(1,0)],
+                    ' -':[(1,0)], ' |':[(0,1)],
+                    'v|':[(0,1)], '>|':[(1,0)],
+                    'v-':[(0,1)], '>-':[(1,0)],
+                    }.get(grid[x,y]+d, []):
+                union(uf, (x,y), (x+dx,y+dy))
+            # We change alll <>v^ to x.
+            tube_grid[x,y] = {
+                    '/-':'┐', '\\-':'┌',
+                    '/|':'└', '\\|':'┘',
+                    ' -':'-', ' |':'|',
+            }.get(grid[x,y]+d, 'x')
+            # We change direction on v and ^, but not on < and >.
+            if grid[x,y] in '\\/v^':
+                d = '|' if d == '-' else '-'
+    return tube_grid, uf
+
+
+def color_tubes(grid):
+    """ Add colors and numbers for drawing the grid to the terminal. """
+    from colorama import Fore, Back, Style
+    tube_grid, uf = make_tubes(grid)
+    letters = string.digits[1:] + string.ascii_letters
+    colors = [Fore.BLUE, Fore.RED, Fore.WHITE, Fore.GREEN, Fore.YELLOW, Fore.MAGENTA, Fore.CYAN, Fore.BLACK]
+    colors = colors + [c+Style.BRIGHT for c in colors]
+    char = collections.defaultdict(lambda: letters[len(char)])
+    col = collections.defaultdict(lambda: colors[len(col)%len(colors)])
+    for x in range(tube_grid.w):
+        for y in range(tube_grid.h):
+            if tube_grid[x,y] == 'x':
+                tube_grid[x,y] = char[find(uf,(x,y))]
+            tube_grid[x,y] = col[find(uf,(x,y))] + tube_grid[x,y] + Style.RESET_ALL + Fore.RESET
+    return tube_grid
+
+
+def add_path(x0, y0, xn, yn, grid, tries=1):
+    for i in range(tries):
+        if grid.add(make_path(x0, y0, xn, yn, grid)):
+            return 1+i
+    return False
+
+
+def has_loops(grid, uf):
+    groups = len({find(uf, (x,y)) for y in range(grid.h) for x in range(grid.w)})
+    ends = sum(bool(grid[x,y] in 'v^<>') for y in range(grid.h) for x in range(grid.w))
+    return ends != 2*groups
+
+
+def has_square(grid, uf):
+    for y in range(grid.h-1):
+        for x in range(grid.w-1):
+            if find(uf, (x,y)) == find(uf, (x+1,y)) == find(uf, (x,y+1)) == find(uf, (x+1,y+1)):
+                return True
+    return False
+
+
+def has_pair(tg, uf):
+    for y in range(tg.h):
+        for x in range(tg.w):
+            for dx,dy in ((1,0), (0,1)):
+                x1, y1 = x + dx, y + dy
+                if x1 < tg.w and y1 < tg.h:
+                    if tg[x,y] == tg[x1,y1] == 'x' and find(uf, (x,y)) == find(uf, (x1,y1)):
+                        return True
+    return False
+
+
+def has_tripple(tg, uf):
+    for y in range(tg.h):
+        for x in range(tg.w):
+            if tg[x,y] != 'x':
+                continue
+            r = find(uf, (x,y))
+            nbs = 0
+            for dx, dy in ((1,0), (0,1), (-1,0), (0,-1)):
+                x1, y1 = x + dx, y + dy
+                if 0 <= x1 < tg.w and 0 <= y1 < tg.h and find(uf, (x1,y1)) == r:
+                    nbs += 1
+            if nbs >= 2:
+                return True
+    return False
+
+
+def make(w, h, min_numbers=0, max_numbers=100):
+    """ Creates a grid of size  w x h  without any loops or squares. """
+
+    # Internally we work on a double size grid to handle crossings
+    grid = Grid(2*w+1, 2*h+1)
+
+    debug('Preprocessing...')
+    mitm = Mitm(lr_price=2, t_price=1)
+    mitm.prepare(h)
+    debug('Generating puzzle...')
+
+    gtries = 0
+    while True:
+        # Previous tries may have drawn stuff on the grid
+        grid.grid.clear()
+
+        # Add left side path
+        path = mitm.rand_path(h, h, 0, -1)
+        if not test_path(grid, path, 0, 0):
+            continue
+        draw_path(grid, path, 0, 0)
+        # Draw_path doesn't know what to put in the first and last squares
+        grid[0,0], grid[0,2*h] = '\\', '/'
+
+        # Add right side path
+        path2 = mitm.rand_path(h, h, 0, -1)
+        if not test_path(grid, path2, 2*w, 2*h, 0, -1):
+            continue
+        draw_path(grid, path2, 2*w, 2*h, 0, -1)
+        grid[2*w,0], grid[2*w,2*h] = '/', '\\'
+
+        # Add loops in the middle
+        # Tube version of full grid, using for tracking orientations.
+        # This doesn't make so much sense in terms of normal numberlink tubes.
+        tg, _ = make_tubes(grid)
+        # Maximum number of tries before retrying main loop
+        for tries in range(1000):
+            x, y = 2*random.randrange(w), 2*random.randrange(h)
+
+            # If the square square doen't have an orientation, it's a corner
+            # or endpoint, so there's no point trying to add a loop there.
+            if tg[x,y] not in '-|':
+                continue
+
+            path = mitm.rand_loop(clock=1 if tg[x,y] == '-' else -1)
+            if test_path(grid, path, x, y):
+                new_path = Grid(2*w+1, 2*h+1)
+                draw_path(new_path, path, x, y, loop=True)
+
+                # A loop may not overlap with anything, and may even have
+                # the right orientation, but if it 'traps' something inside it, that
+                # might now have the wrong orientation.
+                # Hence we clear the insides.
+                inner = (key for key, val in make_tubes(new_path)[0] if val == '|')
+                for key in inner:
+                    grid.grid.pop(key, None)
+
+                # Add path and recompute orientations
+                grid.add(new_path)
+                tg, _ = make_tubes(grid)
+                sg = shrink_grid(grid)
+                stg, uf = make_tubes(sg)
+
+                numbers = list(stg.grid.values()).count('x')//2
+                if numbers > max_numbers:
+                    break
+
+                # Run tests to see if the puzzle is nice
+                if not has_square(sg, uf) \
+                        and not has_loops(sg, uf) \
+                        and not has_pair(stg, uf) \
+                        and not has_tripple(stg, uf) \
+                        and numbers >= min_numbers:
+                    debug(f'Finished in {tries} tries.')
+                    debug(f'{numbers} numbers')
+                    return shrink_grid(grid)
+
+        debug(grid)
+        debug(f'Gave up after {tries} tries')
+
+
+def solution_lines(grid):
+    # Assumes that no two end points are adjacent
+    tg, uf = make_tubes(grid)
+    done = {}
+    for y in range(tg.h):
+        for x in range(tg.w):
+            if tg[x,y] == 'x':
+                r = find(uf, (x,y))
+                if r in done:
+                    continue
+                # Trace
+                line = [(x,y)]
+                while not (len(line) >= 2 and tg[line[-1]] == 'x'):
+                    for dx, dy in ((-1,0),(1,0),(0,1),(0,-1)):
+                        x1, y1 = line[-1][0]+dx, line[-1][1]+dy
+                        if 0 <= x1 < tg.w and 0 <= y1 < tg.w \
+                                and find(uf,(x1,y1)) == r \
+                                and (len(line) == 1 or (x1,y1) != line[-2]):
+                            line.append((x1,y1))
+                            break
+                done[r] = line
+    return done.values()
+
+
+def draw_puzzle(grid, include_solution=False):
+    import matplotlib.pyplot as plt
+    tg, uf = make_tubes(grid)
+
+    # Draw a grid
+    middle_linestyle = dict( lw=1, color='grey', linestyle='--')
+    end_linestyle = dict( lw=2, color='black',)
+    for y in range(tg.h+1):
+        plt.gca().add_line(plt.Line2D((-1/2, tg.w-1/2), (y-1/2,y-1/2),
+            **(middle_linestyle if y not in (0,tg.h) else end_linestyle)))
+    for x in range(tg.w+1):
+        plt.gca().add_line(plt.Line2D((x-1/2,x-1/2), (-1/2, tg.h-1/2),
+            **(middle_linestyle if x not in (0,tg.w) else end_linestyle)))
+
+    # Draw the solution
+    if include_solution:
+        linestyle = dict( lw=3, color='cornflowerblue', solid_joinstyle='round')
+        for line in solution_lines(grid):
+            plt.gca().add_line(plt.Line2D(*zip(*line), **linestyle))
+
+    # Compute maximum number, used for font-size
+    number = collections.defaultdict(lambda: len(number))
+    for y in range(tg.h):
+        for x in range(tg.w):
+            if tg[x,y] == 'x':
+                number[find(uf, (x,y))]
+    max_len = len(str(len(number)))
+
+    # Draw the numbers
+    for y in range(tg.h):
+        for x in range(tg.w):
+            if tg[x,y] == 'x':
+                n = number[find(uf, (x,y))] + 1
+                plt.gca().add_artist(plt.Circle((x,y),1/3,color='white',zorder=2))
+                plt.text(x, y-.1, str(n),
+                        ha='center', va='center',
+                        fontsize=16 if max_len > 1 else 22,
+                        family='fantasy'
+                        )
+
+    plt.axis('scaled')
+    plt.axis('off')
+    plt.show()
+
+
+def debug(s):
+    if args.verbose:
+        print(s, file=sys.stderr)
+
+
+def main():
+    w, h = args.width, args.height
+    n = int((w*h)**.5)
+    min_numbers = n*2//3 if args.min < 0 else args.min
+    max_numbers = n*3//2 if args.max < 0 else args.max
+    grid = make(w, h, min_numbers, max_numbers)
+    color_grid = color_tubes(grid)
+
+    # Print stuff
+    debug(grid)
+
+    print(w, h)
+    if args.zero:
+        # Print puzzle in 0 format
+        for y in range(color_grid.h):
+            for x in range(color_grid.w):
+                if grid[x,y] in 'v^<>':
+                    print(color_grid[x,y], end=' ')
+                else: print('0', end=' ')
+            print()
+    else:
+        for y in range(color_grid.h):
+            for x in range(color_grid.w):
+                if grid[x,y] in 'v^<>':
+                    print(color_grid[x,y], end='')
+                else: print('.', end='')
+            print()
+
+    # Draw with pyplot
+    if args.solve:
+        print(color_grid)
+        draw_puzzle(grid, include_solution=True)
+    else:
+        draw_puzzle(grid)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ gen/mitm.py
@@ -0,0 +1,137 @@
+import random
+from collections import Counter, defaultdict
+import itertools
+
+# starter altid (0,0) -> (0,1)
+# Sti har formen [2, l, r]*, da man kan forlænge med 2, gå til venstre eller gå til højre.
+T, L, R = range(3)
+
+class Path:
+    def __init__(self, steps):
+        self.steps = steps
+
+    def xys(self, dx=0, dy=1):
+        """ Yields all positions on path """
+        x, y = 0, 0
+        yield (x,y)
+        for step in self.steps:
+            x, y = x+dx, y+dy
+            yield (x,y)
+            if step == L:
+                dx, dy = -dy, dx
+            if step == R:
+                dx, dy = dy, -dx
+            elif step == T:
+                x, y = x+dx, y+dy
+                yield (x,y)
+
+    def test(self):
+        """ Tests path is non-overlapping. """
+        ps = list(self.xys())
+        return len(set(ps)) == len(ps)
+
+    def test_loop(self):
+        """ Tests path is non-overlapping, except for first and last. """
+        ps = list(self.xys())
+        seen = set(ps)
+        return len(ps) == len(seen) or len(ps) == len(seen)+1 and ps[0] == ps[-1]
+
+    def winding(self):
+        return self.steps.count(R) - self.steps.count(L)
+
+    def __repr__(self):
+        """ Path to string """
+        return ''.join({T:'2',R:'R',L:'L'}[x] for x in self.steps)
+
+    def show(self):
+        import matplotlib.pyplot as plt
+        xs, ys = zip(*self.xys())
+        plt.plot(xs, ys)
+        plt.axis('scaled')
+        plt.show()
+
+def unrotate(x, y, dx, dy):
+    """ Inverse rotate x, y by (dx,dy), where dx,dy=0,1 means 0 degrees.
+        Basically rotate(dx,dy, dx,dy) = (0, 1). """
+    while (dx, dy) != (0, 1):
+        x, y, dx, dy = -y, x, -dy, dx
+    return x, y
+
+class Mitm:
+    def __init__(self, lr_price, t_price):
+        self.lr_price = lr_price
+        self.t_price = t_price
+        self.inv = defaultdict(list)
+        self.list = []
+
+    def prepare(self, budget):
+        dx0, dy0 = 0, 1
+        for path, (x,y,dx,dy) in self._good_paths(0, 0, dx0, dy0, budget):
+            self.list.append((path, x, y, dx, dy))
+            self.inv[x, y, dx, dy].append(path)
+
+    def _good_paths(self, x, y, dx, dy, budget, seen=None):
+        if seen is None:
+            seen = set()
+        if budget <= 0:
+            yield (), (x,y,dx,dy)
+            return
+        seen.add((x, y)) # Remember cleaning this up (A)
+        x1, y1 = x+dx, y+dy
+        if (x1, y1) not in seen:
+            for path, end in self._good_paths(x1, y1, -dy, dx, budget-self.lr_price, seen):
+                yield (L,) + path, end
+            for path, end in self._good_paths(x1, y1, dy, -dx, budget-self.lr_price, seen):
+                yield (R,) + path, end
+            seen.add((x1, y1)) # Remember cleaning this up (B)
+            x2, y2 = x1+dx, y1+dy
+            if (x2, y2) not in seen:
+                for path, end in self._good_paths(x2, y2, dx, dy, budget-self.t_price, seen):
+                    yield (T,) + path, end
+            seen.remove((x1, y1)) # Clean up (B)
+        seen.remove((x, y)) # Clean up (A)
+
+    def rand_path(self, xn, yn, dxn, dyn):
+        """ Returns a path, starting at (0,0) with dx0,dy0
+            and ending at (xn,yn) with direction (dxn, dyn) """
+        while True:
+            path, x, y, dx, dy = random.choice(self.list)
+            # Give me a path, pointing in direction (0,1) such that when I rotate
+            # it to (dx, dy) it ends at xn-x, yn-y in direction dxn, dyn.
+            xt, yt = unrotate(xn-x, yn-y, dx, dy)
+            dxt, dyt = unrotate(dxn, dyn, dx, dy)
+            path2s = self.inv[xt, yt, dxt, dyt]
+            if path2s:
+                path2 = random.choice(path2s)
+                joined = Path(path+path2)
+                if joined.test():
+                    return joined
+
+    def rand_loop(self, clock=0):
+        """ Set clock = 1 for clockwise, -1 for anti clockwise. 0 for don't care. """
+        while True:
+            # The list only contains 0,1 starting directions
+            path, x, y, dx, dy = random.choice(self.list)
+            # Look for paths ending with the same direction
+            xt, yt = unrotate(-x, -y, dx, dy)
+            dxt, dyt = unrotate(0, 1, dx, dy)
+            path2s = self.inv[xt, yt, dxt, dyt]
+            if path2s:
+                path2 = random.choice(path2s)
+                joined = Path(path+path2)
+                # A clockwise path has 4 R's more than L's.
+                if clock and joined.winding() != clock * 4:
+                     continue
+                if joined.test_loop():
+                    return joined
+
+
+if __name__ == '__main__':
+    mitm = Mitm(1, 1)
+    mitm.prepare(10)
+    for i in range(1):
+        show(mitm.rand_loop())
+    for i in range(1, 10):
+        show(mitm.rand_path(i, i, 0, 1))
+
+
